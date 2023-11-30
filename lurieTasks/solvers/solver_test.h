1663579d6d336c61f42da28ebe1cf0975c3e18f7
alexbibich <2marussia2@gmail.com>
2023-11-30 23:08:01 +0300

    Solver Newton with method solve

--- lurieTasks/solvers/solver_test.h
+++ lurieTasks/solvers/solver_test.h
@@ -15,6 +15,21 @@
 
 using namespace std;
 
+/// @brief Расчёт коэффициента лямбда
+/// @param pipe Структура трубы
+/// @param oil Структура нефти
+/// @param speed Скорость в трубопроводе
+/// @return 
+double getLambda(const pipe_properties_t& pipe, const oil_parameters_t& oil, double speed)
+{
+	// Расчёт относительной шероховатости
+	double eps = pipe.wall.getCompressionRatio();
+	// Расчёт числа Рейнольдса
+	double Re = speed * pipe.wall.diameter / oil.viscosity.nominal_viscosity;
+	// Расчёт коэффициента лямбда
+	return hydraulic_resistance_isaev(Re, eps);
+}
+
 /// @brief Класс для решения задач QP
 class QP_tasks_solver
 {
@@ -25,15 +40,11 @@ public:
 	/// @param direction Означает направление расчёта
 	/// @return Возвращает неизвестное давление
 	double QP_formula_solve(double p, double Q, bool direction = true)
-	{
-		// Расчёт относительной шероховатости
-		double eps = pipe.wall.getCompressionRatio();           
+	{   
 		// Расчёт скорости
-		double speed = 4 * Q / (PI * pow(pipe.wall.diameter, 2));          
-		// Расчёт числа Рейнольдса
-		double Re = speed * pipe.wall.diameter / oil.viscosity.nominal_viscosity; 
+		double speed = 4 * Q / (PI * pow(pipe.wall.diameter, 2));     
 		// Расчёт коэффициента лямбда
-		double lambda = hydraulic_resistance_isaev(Re, eps); 
+		double lambda = getLambda(pipe, oil, speed);
 		// Длина трубопровода
 		double L = pipe.profile.coordinates.back() - pipe.profile.coordinates.front();
 		// Перепал высот
@@ -55,12 +66,8 @@ public:
 	/// @return Профиль давления
 	vector<double> QP_Euler_solver(double p, double speed, bool direction = true)
 	{
-		// Расчёт относительной шероховатости
-		double eps = pipe.wall.getCompressionRatio();
-		// Расчёт числа Рейнольдса
-		double Re = speed * pipe.wall.diameter / oil.viscosity.nominal_viscosity;
-		// Расчёт коэффициента лямбда
-		double lambda = hydraulic_resistance_isaev(Re, eps);
+		//Расчёт коэффициента лямбда
+		double lambda = getLambda(pipe, oil, speed);
 		// Плотность (для краткости записи)
 		double rho = oil.density.nominal_density;
 		// Перепад высот между двумя ближайшими точками (принимаем что перепад высот постоянен по всему трубопроводу)
@@ -100,19 +107,57 @@ protected:
 
 };
 
+double PP_Iteration_solve(const pipe_properties_t& pipe, const oil_parameters_t& oil, double p0, double pl)
+{
+	double lym_b, lambda, Re, speed;
+	// Расчёт относительной шероховатости
+	double eps = pipe.wall.getCompressionRatio();
+	// Ограничитель итераций
+	size_t itr_stop = 0;
+	size_t itr_max = 1000;
+
+	// Для компактности записей
+	double L = pipe.profile.coordinates.back() - pipe.profile.coordinates.front();
+	double d = pipe.wall.diameter;
+	double visc = oil.viscosity.nominal_viscosity;
+	double rho = oil.density.nominal_density;
+	double z0 = pipe.profile.heights.front();
+	double zl = pipe.profile.heights.back();
+
+	// Правая часть уравнения
+	double a = 2 * g * d / L * ((p0 - pl) / (rho * g) + z0 - zl);
+
+	lambda = 0.02;
+	do
+	{
+		itr_stop++;
+		if (itr_stop > itr_max)
+			throw std::runtime_error("Reached maximum number of iterations");
+
+		lym_b = lambda;
+		speed = sqrt(a / lambda);
+		Re = speed * d / visc;
+		lambda = hydraulic_resistance_isaev(Re, eps);
+
+	} while (abs(lambda - lym_b) > 0.000005);
+
+	double Q = PI * pow(d, 2) * speed / 4;
+
+	return Q;
+}
+
 /// @brief Класс с функцией невязок для решения методом Ньютона
 class solver_Newton : public fixed_system_t<1>
 {
 public:
-	solver_Newton(const pipe_properties_t& pipe_t, const oil_parameters_t& oil_t, double p0, double pl, double epsilon)
-		: pipe{ pipe_t }, oil{ oil_t }, p{ p0, pl }, eps{ epsilon }
+	solver_Newton(const pipe_properties_t& pipe_t, const oil_parameters_t& oil_t, double p0, double pl)
+		: pipe{ pipe_t }, oil{ oil_t }, p{ p0, pl }
 	{
 	}
 
 	double residuals(const double& v) {
-		// Расчёт числа Рейнольдса
-		double Re = v * pipe.wall.diameter / oil.viscosity.nominal_viscosity;
-		double lambda = hydraulic_resistance_isaev(Re, eps);
+		// Расчёт коэффициента лямбда
+		double lambda = getLambda(pipe, oil, v);
 		// Расчёт длины трубопровода
 		double L = pipe.profile.coordinates.back() - pipe.profile.coordinates.front();
 
@@ -122,11 +167,30 @@ public:
 		double result = dH + HL - H0; // Задание функции невязок
 		return result;
 	}
+
+	/// @brief Решение задачи РР методом Ньютона
+	/// @param p0 Давление в начале трубопровода
+	/// @param pl Давление в конце трубопровода
+	/// @return Возвращает расход
+	double solve()
+	{
+		double eps = pipe.wall.getCompressionRatio();
+		// Задание настроек решателя по умолчанию
+		fixed_solver_parameters_t<1, 0> parameters;
+		// Создание структуры для записи результатов расчета
+		fixed_solver_result_t<1> result;
+		// Решение системы нелинейныйх уравнений <1> с помощью решателя Ньютона - Рафсона
+		// { 0, 0 } - Начальное приближение
+		fixed_newton_raphson<1>::solve_dense(*this, { 1 }, parameters, &result);
+
+		double Q = result.argument * PI * pow(pipe.wall.diameter, 2) / 4;
+
+		return Q;
+	}
 protected:
 	const pipe_properties_t& pipe;
 	const oil_parameters_t& oil;
 	double p[2];
-	double eps;
 };
 
 /// @brief Солвер для Ньютона поверх Эйлера
@@ -135,10 +199,28 @@ class solver_Newton_Euler : public fixed_system_t<1>
 public:
 	double residuals(const double& v) {
 		QP_tasks_solver euler_solver(pipe, oil);
-		vector<double> press_prof = euler_solver.QP_Euler_solver(p[1], v, dir);
+		vector<double> press_prof = euler_solver.QP_Euler_solver(p[1], v, false);
 		// Задание функции невязок
 		double result = press_prof[0] - p[0]; 
 		return result;
+	}
+
+	/// @brief Решение задачи PP Ньютоном поверх Эйлера
+	/// @param p0 Давление в начале трубопровода
+	/// @param pl Давление в конце трубопровода
+	/// @return Возвращает расход
+	double solve()
+	{
+		fixed_solver_parameters_t<1, 0> parameters;
+		// Создание структуры для записи результатов расчета
+		fixed_solver_result_t<1> result;
+		// Решение системы нелинейныйх уравнений <1> с помощью решателя Ньютона - Рафсона
+		// { 0, 0 } - Начальное приближение
+		fixed_newton_raphson<1>::solve_dense(*this, { 1 }, parameters, &result);
+
+		double Q = result.argument * PI * pow(pipe.wall.diameter, 2) / 4;
+
+		return Q;
 	}
 
 	/// @brief Конструктор солвера
@@ -157,104 +239,6 @@ protected:
 	const oil_parameters_t& oil;
 	double p[2];
 	bool dir;
-};
-
-/// @brief Солвер для решения задач PP
-class PP_tasks_solver
-{
-public:
-	/// @brief Решение задачи PP методом простых итераций
-	/// @param p0 Давление в начале трубопровода
-	/// @param pl Давление в конце трубопровода
-	/// @return Возвращает расход
-	double PP_Iteration_solve(double p0, double pl)
-	{
-		double lym_b, lambda, Re, speed;
-		// Расчёт относительной шероховатости
-		double eps = pipe.wall.getCompressionRatio(); 
-		// Ограничитель итераций
-		size_t itr_stop = 0; 
-		size_t itr_max = 1000;
-		
-		// Для компактности записей
-		double L = pipe.profile.coordinates.back() - pipe.profile.coordinates.front();
-		double d = pipe.wall.diameter;
-		double visc = oil.viscosity.nominal_viscosity;
-		double rho = oil.density.nominal_density;
-		double z0 = pipe.profile.heights.front();
-		double zl = pipe.profile.heights.back();
-
-		// Правая часть уравнения
-		double a = 2 * g * d / L * ((p0 - pl) / (rho * g) + z0 - zl); 
-
-		lambda = 0.02;
-		do
-		{
-			itr_stop++;
-			if (itr_stop > itr_max)
-				throw std::runtime_error("Reached maximum number of iterations");
-
-			lym_b = lambda;
-			speed = sqrt(a / lambda);
-			Re = speed * d / visc;
-			lambda = hydraulic_resistance_isaev(Re, eps);
-
-		} while (abs(lambda - lym_b) > 0.000005);
-
-		double Q = PI * pow(d, 2) * speed / 4;
-
-		return Q;
-	}
-
-	/// @brief Решение задачи РР методом Ньютона
-	/// @param p0 Давление в начале трубопровода
-	/// @param pl Давление в конце трубопровода
-	/// @return Возвращает расход
-	double PP_Newton_solve(double p0, double pl)
-	{
-		double eps = pipe.wall.getCompressionRatio();
-		solver_Newton test(pipe, oil, p0, pl, eps);
-		// Задание настроек решателя по умолчанию
-		fixed_solver_parameters_t<1, 0> parameters;
-		// Создание структуры для записи результатов расчета
-		fixed_solver_result_t<1> result;
-		// Решение системы нелинейныйх уравнений <1> с помощью решателя Ньютона - Рафсона
-		// { 0, 0 } - Начальное приближение
-		fixed_newton_raphson<1>::solve_dense(test, { 1 }, parameters, &result);
-
-		double Q = result.argument * PI * pow(pipe.wall.diameter, 2) / 4;
-
-		return Q;
-	}
-
-	/// @brief Решение задачи PP Ньютоном поверх Эйлера
-	/// @param p0 Давление в начале трубопровода
-	/// @param pl Давление в конце трубопровода
-	/// @return Возвращает расход
-	double PP_Newton_Euler_solve(double p0, double pl)
-	{
-		solver_Newton_Euler solver(pipe, oil, p0, pl);
-		// Задание настроек решателя по умолчанию
-		fixed_solver_parameters_t<1, 0> parameters;
-		// Создание структуры для записи результатов расчета
-		fixed_solver_result_t<1> result;
-		// Решение системы нелинейныйх уравнений <1> с помощью решателя Ньютона - Рафсона
-		// { 0, 0 } - Начальное приближение
-		fixed_newton_raphson<1>::solve_dense(solver, { 1 }, parameters, &result);
-
-		double Q = result.argument * PI * pow(pipe.wall.diameter, 2) / 4;
-
-		return Q;
-	}
-
-	PP_tasks_solver(const pipe_properties_t& pipe_m, const oil_parameters_t& oil_m)
-		: pipe{ pipe_m }, oil{ oil_m }
-	{
-	}
-
-protected:
-	const pipe_properties_t& pipe;
-	const oil_parameters_t& oil;
 };
 
 
